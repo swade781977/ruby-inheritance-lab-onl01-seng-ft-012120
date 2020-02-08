8ca212a9bb99f807e312d2f4ddbc0e03bfa7c5ca
Chris Wade <swade781977@yahoo.com>
2020-02-08 02:28:24 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,10 @@
 class User 
+  
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
+    @@all << self
   end
   attr_accessor :first_name, :last_name
 end
